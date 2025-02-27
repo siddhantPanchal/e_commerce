62fdcf43ebc7664d3cef7a57a376fe929d835659
siddhantPanchal <siddhantpanchal430@gmail.com>
2025-02-27 21:39:56 +0530

    init

---
+++ src/main/kotlin/io/siddhant/e_commerce/SimpleECommerceApiApplication.kt
@@ -0,0 +1,11 @@
+package io.siddhant.e_commerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleECommerceApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SimpleECommerceApiApplication>(*args)
+}
---
+++ src/test/kotlin/io/siddhant/e_commerce/SimpleECommerceApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.siddhant.e_commerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleECommerceApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
